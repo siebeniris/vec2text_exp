f339a59ba0ddb6355d39b84796f7c70efb008ce5
yiyi chen <chen.yiyi@protonmail.com>
2024-06-13 23:12:05 +0200

    experiments property

--- vec2text/__init__.py
+++ vec2text/__init__.py
@@ -6,6 +6,7 @@ from . import (  # noqa: F401; analyze_utils,
     prompts,
     trainers,
     trainers_baseline,
+    analyze_utils
 )
 from .api import invert_embeddings, invert_strings, load_corrector  # noqa: F401
 from .trainers import Corrector  # noqa: F401
