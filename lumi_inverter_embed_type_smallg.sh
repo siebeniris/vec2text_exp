b6b96f2eaf1d2e8ff7153138bcd8a16e2436b636
yiyi chen <chen.yiyi@protonmail.com>
2024-06-16 01:43:54 +0200

    embed output type

--- lumi_inverter_embed_type_smallg.sh
+++ lumi_inverter_embed_type_smallg.sh
@@ -110,7 +110,7 @@ if [ $OVERWRITE_OUTPUT_DIR -eq 1 ]; then
           --learning_rate ${LEARNING_RATE} \
           --ddp_find_unused_parameters True \
           --use_frozen_embeddings_as_input True \
-          --whitening first_last \
+          --embedding_output first_last \
           --overwrite_output_dir"
 else
   echo "no overwrite parameters"
@@ -130,6 +130,6 @@ else
           --apply_early_stopping_metric ${EARLY_STOPPING} \
           --ddp_find_unused_parameters True \
           --use_frozen_embeddings_as_input True \
-          --whitening first_last \
+          --embedding_output first_last \
           --learning_rate ${LEARNING_RATE} "
 fi
