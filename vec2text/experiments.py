75ae7c655f7ec9d1128434428c9655eb80a8ed8a
yiyi chen <chen.yiyi@protonmail.com>
2024-06-25 23:04:08 +0200

    vec2text

--- vec2text/experiments.py
+++ vec2text/experiments.py
@@ -49,7 +49,6 @@ os.environ["_WANDB_STARTUP_DEBUG"] = "true"
 os.environ["TOKENIZERS_PARALLELISM"] = "False"
 # os.environ["TOKENIZERS_PARALLELISM"] = "True"
 
-device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 logger = logging.getLogger(__name__)
 
 # We maintain our own cache because huggingface datasets caching
@@ -58,14 +57,17 @@ cwd = os.getcwd()
 DATASET_CACHE_PATH = os.environ.get(
     "VEC2TEXT_CACHE", os.path.expanduser(f"{cwd}/.cache/inversion")
 )
-
-rank = int(os.environ["RANK"])
-local_rank = int(os.environ["LOCAL_RANK"])
-world_size = int(os.environ["WORLD_SIZE"])
-local_world_size = int(os.environ["LOCAL_WORLD_SIZE"])
-device = torch.device("cuda", local_rank)
-# print("lumi set the cpu affinity...")
-set_cpu_affinity_lumi(local_rank)
+if os.getenv("RANK"):
+    rank = int(os.environ["RANK"])
+    local_rank = int(os.environ["LOCAL_RANK"])
+    world_size = int(os.environ["WORLD_SIZE"])
+    local_world_size = int(os.environ["LOCAL_WORLD_SIZE"])
+    device = torch.device("cuda", local_rank)
+    # print("lumi set the cpu affinity...")
+    set_cpu_affinity_lumi(local_rank)
+else:
+    device = torch.device(
+        "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 
 
 # Noisy compilation from torch.compile
