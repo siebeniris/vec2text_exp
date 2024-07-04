0efdadb79abd159926f7a6feada19d8106760691
Yiyi Chen <chen.yiyi@protonmail.com>
2024-07-04 11:51:52 +0200

    claudiaa eval

---
+++ claudiaa_eval.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -e
+
+#SBATCH --job-name=eval
+#SBATCH --output=eval_%j.out
+#SBATCH --error=eval_%j.err
+#SBATCH --mem=100GB
+#SBATCH --time=2-00:00:00
+
+set -x
+
+MODEL_NAME=$1
+BATCH_SIZE=$2
+
+
+wd=$(pwd)
+echo "working directory ${wd}"
+
+export HF_HOME="${wd}/.cache"
+export HF_DATASETS_CACHE="${wd}/.cache/datasets"
+export DATASET_CACHE_PATH="${wd}/.cache"
+export WANDB_CACHE_DIR="${wd}/.cache/wandb/artifcats/"
+
+
+export NCCL_P2P_LEVEL=NVL
+export NCCL_IB_DISABLE=1
+export TORCH_NCCL_ENABLE_MONITORING=0
+# tokenization timeouts handling
+export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
+export CUDA_VISIBLE_DEVICES=0
+
+
+
+SIF=/home/cs.aau.dk/ng78zb/pytorch_23.10-py3.sif
+echo "sif ${SIF}"
+
+
+echo "launch evaluation ${MODEL_NAME} with batch size ${BATCH_SIZE}"
+
+srun singularity exec --nv --cleanenv --bind ${wd}:${wd} ${SIF} \
+  python -m evaluation ${MODEL_NAME} ${BATCH_SIZE}
