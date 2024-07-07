8844dadfce682f5fa9a26f7486d439256728931f
yiyi chen <chen.yiyi@protonmail.com>
2024-07-07 10:40:39 +0200

    last layer monolingual

--- vec2text/aliases.py
+++ vec2text/aliases.py
@@ -27,16 +27,6 @@ CHECKPOINT_FOLDERS_DICT = {
     "heb_Hebr_32_me5_2layers": f"{lumi_dir}/saves/inverters/mt5_multilingual_e5_base_mt-ms_heb_Hebr_32_2layers_prefix",
     "cmn_Hani_32_me5_2layers": f"{lumi_dir}/saves/inverters/mt5_multilingual_e5_base_mt-ms_cmn_Hani_32_2layers_prefix",
 
-    # monolingual models
-    # last layers
-    "deu_Latn_32_me5_last_layer": f"{lumi_dir}/saves/inverters/mt5_gtr_base_mt-ms_deu_Latn_32_last_layer",
-    "heb_Hebr_32_me5_last_layer": f"{lumi_dir}/saves/inverters/mt5_alephbert_mt-ms_heb_Hebr_32_last_layer",
-    "cmn_Hani_32_me5_last_layer": f"{lumi_dir}/saves/inverters/mt5_text2vec-base-cmn_mt-ms_cmn_Hani_32_last_layer",
-
-    "deu_Latn_32_me5_corrector_last_layer": f"{lumi_dir}/saves/correctors/mt5_gtr_base_mt-ms_deu_Latn_32_last_layer",
-    "heb_Hebr_32_me5_corrector_last_layer": f"{lumi_dir}/saves/correctors/mt5_alephbert_mt-ms_heb_Hebr_32_last_layer",
-    "cmn_Hani_32_me5_corrector_last_layer": f"{lumi_dir}/saves/correctors/mt5_text2vec-base-cmn_mt-ms_cmn_Hani_32_last_layer",
-
     # by family
     "indo-aryan_fami_32_me5_2layers": f"{lumi_dir}/saves/inverters/mt5_multilingual_e5_base_mt-ms_ind_fami_32_2layers_prefix",
     "semitic_fami_32_me5_2layers": f"{lumi_dir}/saves/inverters/mt5_multilingual_e5_base_mt-ms_sem_fami_32_2layers_prefix",
@@ -81,6 +71,7 @@ CHECKPOINT_FOLDERS_DICT = {
 
 
     # monolingual
+    # last layer.
     "deu_Latn_32_gtr_last_layer": f"{lumi_dir}/saves/inverters/mt5_gtr_base_mt-ms_deu_Latn_32_last_layer",
     "heb_Hebr_32_alephbert_last_layer": f"{lumi_dir}/saves/inverters/mt5_alephbert_mt-ms_heb_Hebr_32_last_layer",
     "cmn_Hani_32_text2vec_last_layer": f"{lumi_dir}/saves/inverters/mt5_text2vec-base-cmn_mt-ms_cmn_Hani_32_last_layer",
