cafc05dcc50d7051e81a79bd05527e291141174a
yiyi chen <chen.yiyi@protonmail.com>
2024-07-29 16:10:11 +0800

    uploade models

--- upload_models.sh
+++ upload_models.sh
@@ -1,48 +1,62 @@
 #!/bin/bash -e
 
 
-python -m vec2text.upload_model tur_urd_32_2layers_inverter mt5_me5_tur_urd_32_2layers_inverter
+python -m vec2text.upload_model urd_pan_32_2layers_corrector mt5_me5_urd_pan_32_2layers_corrector
 
 
 sleep 5
 
-python -m vec2text.upload_model tur_pan_32_2layers_inverter mt5_me5_tur_pan_32_2layers_inverter
-
-
-
-sleep 5
-
-python -m vec2text.upload_model tur_guj_32_2layers_inverter mt5_me5_tur_guj_32_2layers_inverter
+python -m vec2text.upload_model urd_guj_32_2layers_corrector mt5_me5_urd_guj_32_2layers_corrector
 
 
 
 sleep 5
 
-python -m vec2text.upload_model tur_hin_32_2layers_inverter mt5_me5_tur_hin_32_2layers_inverter
+python -m vec2text.upload_model urd_hin_32_2layers_corrector mt5_me5_urd_hin_32_2layers_corrector
 
 
 
 sleep 5
 
-python -m vec2text.upload_model kaz_urd_32_2layers_inverter mt5_me5_kaz_urd_32_2layers_inverter
+python -m vec2text.upload_model hin_pan_32_2layers_corrector mt5_me5_hin_pan_32_2layers_corrector
 
 
 
 sleep 5
 
-python -m vec2text.upload_model kaz_pan_32_2layers_inverter mt5_me5_kaz_pan_32_2layers_inverter
+python -m vec2text.upload_model pan_guj_32_2layers_corrector mt5_me5_pan_guj_32_2layers_corrector
+
+
+
+sleep 5
+
+python -m vec2text.upload_model tur_urd_32_2layers_corrector mt5_me5_tur_urd_32_2layers_corrector
 
 
 
 
 sleep 5
 
-python -m vec2text.upload_model kaz_guj_32_2layers_inverter mt5_me5_kaz_guj_32_2layers_inverter
+python -m vec2text.upload_model tur_pan_32_2layers_corrector mt5_me5_tur_pan_32_2layers_corrector
 
 
 
 sleep 5
 
-python -m vec2text.upload_model kaz_hin_32_2layers_inverter mt5_me5_kaz_hin_32_2layers_inverter
+python -m vec2text.upload_model tur_guj_32_2layers_corrector mt5_me5_tur_guj_32_2layers_corrector
 
 
+sleep 5
+
+python -m vec2text.upload_model kaz_pan_32_2layers_corrector mt5_me5_kaz_pan_32_2layers_corrector
+
+
+sleep 5
+
+python -m vec2text.upload_model kaz_guj_32_2layers_corrector mt5_me5_kaz_guj_32_2layers_corrector
+
+
+
+sleep 5
+
+python -m vec2text.upload_model kaz_hin_32_2layers_corrector mt5_me5_kaz_hin_32_2layers_corrector
