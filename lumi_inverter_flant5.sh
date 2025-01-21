fb725c706311af734dfce9294e4e2bc99b6dfdbb
yiyi chen <chen.yiyi@protonmail.com>
2025-01-21 17:18:30 +0100

    lumi

--- lumi_inverter_flant5.sh
+++ lumi_inverter_flant5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #SBATCH --job-name=inverter_baseline
-#SBATCH --account=project_465001270
+#SBATCH --account=project_465000909
 #SBATCH --partition=small-g
 #SBATCH --gpus-per-node=1
 #SBATCH --ntasks-per-node=1
@@ -81,7 +81,7 @@ export NCCL_NET_GDR_LEVEL=3
 # pytorch multiprocessing. semaphore.
 export PYTHONWARNINGS='ignore:semaphore_tracker:UserWarning'
 
-SIF=/scratch/project_465001270/multivec2text.sif
+SIF=/scratch/project_465000909/multivec2text.sif
 
 # each GPU has a mask, communicating with the closest CPUs.
 #CPU_BIND_MASKS="0x00fe000000000000,0xfe00000000000000,0x0000000000fe0000,0x00000000fe000000,0x00000000000000fe,0x000000000000fe00,0x000000fe00000000,0x0000fe0000000000"
