6f5e7a36de010a1cdd18dd9ea5842bfc0f7247ce
yiyi chen <chen.yiyi@protonmail.com>
2025-01-22 12:16:19 +0100

    corrector

--- lumi_corrector_flant5.sh
+++ lumi_corrector_flant5.sh
@@ -12,7 +12,6 @@
 
 
 
-
 set -x
 
 LANG=$1
@@ -89,13 +88,12 @@ export PYTHONWARNINGS='ignore:semaphore_tracker:UserWarning'
 SIF=/scratch/project_465001270/multivec2text.sif
 
 # each GPU has a mask, communicating with the closest CPUs.
-CPU_BIND_MASKS="0x00fe000000000000,0xfe00000000000000,0x0000000000fe0000,0x00000000fe000000,0x00000000000000fe,0x000000000000fe00,0x000000fe00000000,0x0000fe0000000000"
 
 echo $SIF
 chmod +x $HF_HOME
 chmod +x $HF_DATASETS_CACHE
 
-srun --cpu-bind=mask_cpu:$CPU_BIND_MASKS singularity exec \
+srun singularity exec \
     -B /scratch/project_465001270:/scratch/project_465001270 \
     -B ${wd}:${wd} \
     -B ${HF_HOME}:${HF_HOME} \
