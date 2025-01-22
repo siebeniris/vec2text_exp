8d2d5f84c061536a301516882235488617057d06
yiyi chen <chen.yiyi@protonmail.com>
2025-01-22 20:27:30 +0100

    aliases

--- vec2text/aliases.py
+++ vec2text/aliases.py
@@ -18,13 +18,21 @@ CHECKPOINT_FOLDERS_DICT = {
     ######################## Fewshot Baselines #########################
     ####################################################################
     "gtr_multihplt_1000": f"{lumi_dir}/inverters/flant5_sentence-transformers/gtr-t5-base_yiyic/multiHPLT_english_1000_32",
-
     "me5_multihplt_1000": f"{lumi_dir}/inverters/flant5_intfloat/multilingual-e5-base_yiyic/multiHPLT_english_1000_32",
     "flant5base_multihplt_1000": f"{lumi_dir}/inverters/flant5_google/flan-t5-base_yiyic/multiHPLT_english_1000_32",
     "t5_multihplt_1000": f"{lumi_dir}/inverters/flant5_google-t5/t5-base_yiyic/multiHPLT_english_1000_32",
     "mt5_multihplt_1000": f"{lumi_dir}/inverters/flant5_google/mt5-base_yiyic/multiHPLT_english_1000_32",
     "mbert_multihplt_1000": f"{lumi_dir}/inverters/flant5_google-bert/bert-base-multilingual-cased_yiyic/multiHPLT_english_1000_32",
     "sbert_multihplt_1000": f"{lumi_dir}/inverters/flant5_sentence-transformers/all-MiniLM-L6-v2_yiyic/multiHPLT_english_1000_32",
+
+    # correctors.
+    "gtr_multihplt_1000_corrector": f"{lumi_dir}/correctors/flant5_sentence-transformers/gtr-t5-base_yiyic/multiHPLT_english_1000_32",
+    "me5_multihplt_1000_corrector": f"{lumi_dir}/correctors/flant5_intfloat/multilingual-e5-base_yiyic/multiHPLT_english_1000_32",
+    "flant5base_multihplt_1000_corrector": f"{lumi_dir}/correctors/flant5_google/flan-t5-base_yiyic/multiHPLT_english_1000_32",
+    "t5_multihplt_1000_corrector": f"{lumi_dir}/correctors/flant5_google-t5/t5-base_yiyic/multiHPLT_english_1000_32",
+    "mt5_multihplt_1000_corrector": f"{lumi_dir}/correctors/flant5_google/mt5-base_yiyic/multiHPLT_english_1000_32",
+    "mbert_multihplt_1000_corrector": f"{lumi_dir}/",
+    "sbert_multihplt_1000_corrector": f"{lumi_dir}/",
 
     ####################################################################
     ######################## Multilingual e5 base #########################
