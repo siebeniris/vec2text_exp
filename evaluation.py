e04fd33ff1fa8b642d192a7c1c7bb2b81b0733c5
yiyi chen <chen.yiyi@protonmail.com>
2025-01-23 14:39:30 +0100

    eval with 4 and 8 sbeam

--- evaluation.py
+++ evaluation.py
@@ -38,7 +38,7 @@ def eval_and_save_results(trainer, dataset, dataset_name, output_dir, corrector=
             eval_function(trainer, dataset, filepath)
 
         # for sbeam in [4, 8]:
-        for sbeam in [8]:
+        for sbeam in [4, 8]:
             trainer.args.per_device_eval_batch_size = 1
             trainer.num_gen_recursive_steps = 50
             trainer.sequence_beam_width = sbeam
