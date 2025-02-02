ada2a35343ef22ff2cd276c9d43931a801a67c6d
yiyi chen <chen.yiyi@protonmail.com>
2025-02-02 14:03:24 +0100

    lumi upload models script

--- lumi_upload_models.sh
+++ lumi_upload_models.sh
@@ -1,87 +1,200 @@
 #!/bin/bash -e
 
+python -m vec2text.upload_model mbert_multihplt_30 yiyic/mbert_multihplt_30
+timeout /t 2 /nobreak >nul
 
-python -m vec2text.upload_model mbert_multihplt_500 yiyic/mbert_multihplt_500
+################################
+
+
+python -m vec2text.upload_model gtr_multihplt_500_corrector yiyic/gtr_multihplt_500_corrector
+
+timeout /t 2 /nobreak >nul
+
+python -m vec2text.upload_model t5_multihplt_500_corrector yiyic/t5_multihplt_500_corrector
 
 timeout /t 2 /nobreak >nul
 
 
-python -m vec2text.upload_model gtr_multihplt_300 yiyic/gtr_multihplt_300
+python -m vec2text.upload_model mt5_multihplt_500_corrector yiyic/mt5_multihplt_500_corrector
 
 timeout /t 2 /nobreak >nul
 
-python -m vec2text.upload_model t5_multihplt_300 yiyic/t5_multihplt_300
+python -m vec2text.upload_model mbert_multihplt_500_corrector yiyic/mbert_multihplt_500_corrector
 
 timeout /t 2 /nobreak >nul
 
-
-python -m vec2text.upload_model mt5_multihplt_300 yiyic/mt5_multihplt_300
-
-timeout /t 2 /nobreak >nul
+################################
 
 
-python -m vec2text.upload_model mbert_multihplt_300 yiyic/mbert_multihplt_300
+python -m vec2text.upload_model gtr_multihplt_300_corrector yiyic/gtr_multihplt_300_corrector
 
 timeout /t 2 /nobreak >nul
 
-
-python -m vec2text.upload_model gtr_multihplt_100 yiyic/gtr_multihplt_100
+python -m vec2text.upload_model t5_multihplt_300_corrector yiyic/t5_multihplt_300_corrector
 
 timeout /t 2 /nobreak >nul
 
 
-
-python -m vec2text.upload_model t5_multihplt_100 yiyic/t5_multihplt_100
-
-timeout /t 2 /nobreak >nul
-
-
-python -m vec2text.upload_model mt5_multihplt_100 yiyic/mt5_multihplt_100
+python -m vec2text.upload_model mt5_multihplt_300_corrector yiyic/mt5_multihplt_300_corrector
 
 timeout /t 2 /nobreak >nul
 
 
-python -m vec2text.upload_model mbert_multihplt_100 yiyic/mbert_multihplt_100
+python -m vec2text.upload_model mbert_multihplt_300_corrector yiyic/mbert_multihplt_300_corrector
 
 timeout /t 2 /nobreak >nul
 
+################################
 
-
-python -m vec2text.upload_model gtr_multihplt_30 yiyic/gtr_multihplt_30
-
-timeout /t 2 /nobreak >nul
-
-
-
-python -m vec2text.upload_model t5_multihplt_30 yiyic/t5_multihplt_30
+python -m vec2text.upload_model gtr_multihplt_100_corrector yiyic/gtr_multihplt_100_corrector
 
 timeout /t 2 /nobreak >nul
 
 
 
-python -m vec2text.upload_model mt5_multihplt_30 yiyic/mt5_multihplt_30
+python -m vec2text.upload_model t5_multihplt_100_corrector yiyic/t5_multihplt_100_corrector
 
 timeout /t 2 /nobreak >nul
 
 
-python -m vec2text.upload_model gtr_multihplt_10 yiyic/gtr_multihplt_10
+python -m vec2text.upload_model mt5_multihplt_100_corrector yiyic/mt5_multihplt_100_corrector
 
 timeout /t 2 /nobreak >nul
 
 
-python -m vec2text.upload_model t5_multihplt_10 yiyic/t5_multihplt_10
+python -m vec2text.upload_model mbert_multihplt_100_corrector yiyic/mbert_multihplt_100_corrector
+
+timeout /t 2 /nobreak >nul
+
+################################
+
+
+python -m vec2text.upload_model gtr_multihplt_30_corrector yiyic/gtr_multihplt_30_corrector
 
 timeout /t 2 /nobreak >nul
 
 
-python -m vec2text.upload_model mt5_multihplt_10 yiyic/mt5_multihplt_10
+
+python -m vec2text.upload_model t5_multihplt_30_corrector yiyic/t5_multihplt_30_corrector
 
 timeout /t 2 /nobreak >nul
 
 
 
-python -m vec2text.upload_model mbert_multihplt_10 yiyic/mbert_multihplt_10
+python -m vec2text.upload_model mt5_multihplt_30_corrector yiyic/mt5_multihplt_30_corrector
 
 timeout /t 2 /nobreak >nul
 
 
+python -m vec2text.upload_model mbert_multihplt_30_corrector yiyic/mbert_multihplt_30_corrector
+timeout /t 2 /nobreak >nul
+
+
+################################
+python -m vec2text.upload_model gtr_multihplt_10_corrector yiyic/gtr_multihplt_10_corrector
+
+timeout /t 2 /nobreak >nul
+
+
+python -m vec2text.upload_model t5_multihplt_10_corrector yiyic/t5_multihplt_10_corrector
+
+timeout /t 2 /nobreak >nul
+
+
+python -m vec2text.upload_model mt5_multihplt_10_corrector yiyic/mt5_multihplt_10_corrector
+
+timeout /t 2 /nobreak >nul
+
+
+
+python -m vec2text.upload_model mbert_multihplt_10_corrector yiyic/mbert_multihplt_10_corrector
+
+timeout /t 2 /nobreak >nul
+
+
+#
+#
+#python -m vec2text.upload_model mbert_multihplt_500 yiyic/mbert_multihplt_500
+#
+#timeout /t 2 /nobreak >nul
+#
+#
+#python -m vec2text.upload_model gtr_multihplt_300 yiyic/gtr_multihplt_300
+#
+#timeout /t 2 /nobreak >nul
+#
+#python -m vec2text.upload_model t5_multihplt_300 yiyic/t5_multihplt_300
+#
+#timeout /t 2 /nobreak >nul
+#
+#
+#python -m vec2text.upload_model mt5_multihplt_300 yiyic/mt5_multihplt_300
+#
+#timeout /t 2 /nobreak >nul
+#
+#
+#python -m vec2text.upload_model mbert_multihplt_300 yiyic/mbert_multihplt_300
+#
+#timeout /t 2 /nobreak >nul
+#
+#
+#python -m vec2text.upload_model gtr_multihplt_100 yiyic/gtr_multihplt_100
+#
+#timeout /t 2 /nobreak >nul
+#
+#
+#
+#python -m vec2text.upload_model t5_multihplt_100 yiyic/t5_multihplt_100
+#
+#timeout /t 2 /nobreak >nul
+#
+#
+#python -m vec2text.upload_model mt5_multihplt_100 yiyic/mt5_multihplt_100
+#
+#timeout /t 2 /nobreak >nul
+#
+#
+#python -m vec2text.upload_model mbert_multihplt_100 yiyic/mbert_multihplt_100
+#
+#timeout /t 2 /nobreak >nul
+#
+#
+#
+#python -m vec2text.upload_model gtr_multihplt_30 yiyic/gtr_multihplt_30
+#
+#timeout /t 2 /nobreak >nul
+#
+#
+#
+#python -m vec2text.upload_model t5_multihplt_30 yiyic/t5_multihplt_30
+#
+#timeout /t 2 /nobreak >nul
+#
+#
+#
+#python -m vec2text.upload_model mt5_multihplt_30 yiyic/mt5_multihplt_30
+#
+#timeout /t 2 /nobreak >nul
+#
+#
+#python -m vec2text.upload_model gtr_multihplt_10 yiyic/gtr_multihplt_10
+#
+#timeout /t 2 /nobreak >nul
+#
+#
+#python -m vec2text.upload_model t5_multihplt_10 yiyic/t5_multihplt_10
+#
+#timeout /t 2 /nobreak >nul
+#
+#
+#python -m vec2text.upload_model mt5_multihplt_10 yiyic/mt5_multihplt_10
+#
+#timeout /t 2 /nobreak >nul
+#
+#
+#
+#python -m vec2text.upload_model mbert_multihplt_10 yiyic/mbert_multihplt_10
+#
+#timeout /t 2 /nobreak >nul
+#
+#
