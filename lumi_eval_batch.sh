cf1d8d5e1e64d1d53d432eeae77408463ac87315
yiyi chen <chen.yiyi@protonmail.com>
2025-02-02 18:05:56 +0100

    eval batch

---
+++ lumi_eval_batch.sh
@@ -0,0 +1,92 @@
+#!/bin/bash -e
+
+################################ 500
+sbatch lumi_eval.sh yiyic/mbert_multihplt_500_corrector
+
+sbatch lumi_eval.sh yiyic/gtr_multihplt_500_corrector
+
+sbatch lumi_eval.sh yiyic/t5_multihplt_500_corrector
+
+sbatch lumi_eval.sh yiyic/mt5_multihplt_500_corrector
+
+sbatch lumi_eval.sh yiyic/gtr_multihplt_500
+
+sbatch lumi_eval.sh yiyic/mbert_multihplt_500
+
+sbatch lumi_eval.sh yiyic/t5_multihplt_500
+
+sbatch lumi_eval.sh yiyic/mt5_multihplt_500
+
+
+################################ 300
+sbatch lumi_eval.sh yiyic/mbert_multihplt_300_corrector
+
+sbatch lumi_eval.sh yiyic/gtr_multihplt_300_corrector
+
+sbatch lumi_eval.sh yiyic/t5_multihplt_300_corrector
+
+sbatch lumi_eval.sh yiyic/mt5_multihplt_300_corrector
+
+sbatch lumi_eval.sh yiyic/gtr_multihplt_300
+
+sbatch lumi_eval.sh yiyic/mbert_multihplt_300
+
+sbatch lumi_eval.sh yiyic/t5_multihplt_300
+
+sbatch lumi_eval.sh yiyic/mt5_multihplt_300
+
+
+
+################################ 100
+sbatch lumi_eval.sh yiyic/mbert_multihplt_100_corrector
+
+sbatch lumi_eval.sh yiyic/gtr_multihplt_100_corrector
+
+sbatch lumi_eval.sh yiyic/t5_multihplt_100_corrector
+
+sbatch lumi_eval.sh yiyic/mt5_multihplt_100_corrector
+
+sbatch lumi_eval.sh yiyic/gtr_multihplt_100
+
+sbatch lumi_eval.sh yiyic/mbert_multihplt_100
+
+sbatch lumi_eval.sh yiyic/t5_multihplt_100
+
+sbatch lumi_eval.sh yiyic/mt5_multihplt_100
+
+
+################################ 30
+sbatch lumi_eval.sh yiyic/mbert_multihplt_30_corrector
+
+sbatch lumi_eval.sh yiyic/gtr_multihplt_30_corrector
+
+sbatch lumi_eval.sh yiyic/t5_multihplt_30_corrector
+
+sbatch lumi_eval.sh yiyic/mt5_multihplt_30_corrector
+
+sbatch lumi_eval.sh yiyic/gtr_multihplt_30
+
+sbatch lumi_eval.sh yiyic/mbert_multihplt_30
+
+sbatch lumi_eval.sh yiyic/t5_multihplt_30
+
+sbatch lumi_eval.sh yiyic/mt5_multihplt_30
+
+
+
+################################ 10
+sbatch lumi_eval.sh yiyic/mbert_multihplt_10_corrector
+
+sbatch lumi_eval.sh yiyic/gtr_multihplt_10_corrector
+
+sbatch lumi_eval.sh yiyic/t5_multihplt_10_corrector
+
+sbatch lumi_eval.sh yiyic/mt5_multihplt_10_corrector
+
+sbatch lumi_eval.sh yiyic/gtr_multihplt_10
+
+sbatch lumi_eval.sh yiyic/mbert_multihplt_10
+
+sbatch lumi_eval.sh yiyic/t5_multihplt_10
+
+sbatch lumi_eval.sh yiyic/mt5_multihplt_10
